684fe3dff862488ba85ce598fc701c98afa2b27f
Gaurav Dhingra <dhingrag11@gmail.com>
2020-05-27 19:50:25 +0530

    First Commit

---
+++ src/context/todos.context.js
@@ -0,0 +1,23 @@
+import React, { createContext } from 'react';
+import { useLocalStorageReducer } from "../hooks/useLocalStorageReducer";
+import todoReducer from "../reducers/todo.reducer";
+
+const defaultTodos = [
+    { id: 1, task: 'Mow the lawn', completed: false },
+    { id: 2, task: 'Release lady bugs', completed: true },
+]
+
+export const TodosContext = createContext();
+export const DispatchContext = createContext();
+
+export function TodosProvider(props) {
+    // const { todos, addTodo, removeTodo, toggleTodo, editTodo } = useTodoState(defaultTodos);
+    const [todos, dispatch] = useLocalStorageReducer('todos', defaultTodos, todoReducer);
+    return (
+        <TodosContext.Provider value={todos}>
+            <DispatchContext.Provider value={dispatch}>
+                {props.children}
+            </DispatchContext.Provider>
+        </TodosContext.Provider>
+    )
+}
---
+++ src/reducers/todo.reducer.js
@@ -0,0 +1,26 @@
+import uuid from "uuid/v4";
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD':
+            return [...state, { id: uuid(), task: action.task, completed: false }];
+        case 'REMOVE':
+            return state.filter(todo => todo.id !== action.id);
+        case 'TOGGLE':
+            return state.map(todo =>
+                todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+            );
+        case 'EDIT':
+            return state.map(todo =>
+                todo.id === action.id ? { ...todo, task: action.task } : todo
+            );
+        default:
+            return state;
+    }
+}
+
+export default reducer;
+
+// {type:'ADD',task:'Walk the dog'}
+// {type:'REMOVE',id:45}
+// {type:'TOGGLE',id:45}
+// {type:'EDIT',id:45,task:'Walk the dog'}
